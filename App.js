593b7078b318d7b935b4e2c05e97db8b43f34d1a
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-10-24 19:21:51 -0400

    Setup the NativeWind Configurations and fixed the Title Overriding Issue

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 // Components
 import { fetchClubs } from './api/fetchClubs';
-import { Text, View } from 'react-native';
+import { Text, View, Image, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 
 // NativeWind Canvas & Engine
 import { NativeWindStyleSheet } from "nativewind";
@@ -33,14 +33,50 @@ const App = () => {
     fetchData();
   }, []);
 
+  // format:
+  // 1. id
+  // 2. name
+  // 3. address
+  // 4. category
+  // 5. email
+  // 6. leaders: list
+  // 7. logo
+  // 8. mission
+  // 9. phone
+  // 10. type
+  // 11. website
+  // 12. benefits
+  // 13. constitution
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity key={item.id}>
+      <View>
+        <Text className="font-normal mb-1 flex">{item.id}</Text>
+        <Text className="font-normal mb-1">{item.name}</Text>
+        <Text className="font-normal mb-1">{item.email}</Text>
+        <Text className="font-normal mb-1">{item.website}</Text>
+        <Text className="font-normal mb-1">{item.address}</Text>
+        <Text className="font-normal mb-1">{item.category}</Text>
+        <Text className="font-normal mb-1">{item.leaders}</Text>
+        <Image source={{ uri: item.logo }} />
+        <Text className="font-normal mb-1">{item.mission}</Text>
+        <Text className="font-normal mb-1">{item.type}</Text>
+        <Text className="font-normal mb-1">{item.benefits}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    // must wrap in NavigationContainer for React-Navigation to work
     <NavigationContainer documentTitle={{ enabled: false }}>
-      <View className="flex-1 flex items-center justify-center bg-blue-950">
-        <Text className="font-bold text-white mb-2 text-lg">Hello NativeWind! List of Clubs:</Text>
-        {groups.map((group, index) => (
-          <Text className="font-normal mb-1 text-white" key={index}>{group.name}</Text>
-        ))}
+      <View className="bg-gray-200">
+        <Text className="font-bold mb-5 text-lg">List of Clubs:</Text>
+        <SafeAreaView>
+          <FlatList
+            data={groups}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </SafeAreaView>
       </View>
     </NavigationContainer>
   );
