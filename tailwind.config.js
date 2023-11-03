08f43902afa904374b71ce237f42a652f38c8d94
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-11-03 17:05:52 -0400

    Full Main Component
    Main Changes Include:
    1. Implemented Main Component JSX
    2. Implemented Clever Search Bar
    3. SetUp Fetch/Filters on API, uploaded data, roughly visualized
    4. SetUp Infinite Loading for Data
    5. Fixed Scroll Issue w\ Expo
    6. Implemented Folder Structure for Further Team Development of the Main Page
    7. Changes Logo (Improved Quality)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
