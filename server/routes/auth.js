11ea4cd62cbbd2e3cb06a67d275786562f27fe32
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-03 01:03:25 -0500

    ht

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const axios = require("axios")
 const { XMLParser} = require("fast-xml-parser");
 
-const CAS_SERVER = 'https://secure.its.yale.edu'
-const CAS_VALIDATE_ROUTE = '/cas/serviceValidate'
-const CAS_SERVICE = "http://localhost:8081/api/auth/redirect"
+const router = express.Router();
+
+const CAS_SERVER = 'https://secure.its.yale.edu';
+const CAS_VALIDATE_ROUTE = '/cas/serviceValidate';
+const CAS_SERVICE = "http://localhost:8081/api/auth/redirect";
 
 const get_ticket_validation_link = (ticket) => {
     const validateURL = new URL(CAS_VALIDATE_ROUTE, CAS_SERVER)
@@ -14,17 +15,24 @@ const get_ticket_validation_link = (ticket) => {
     return validateURL.toString()
 }
 
-router.get("/auth/redirect", async (req, res) => {
-    const response = await axios.get(get_ticket_validation_link(req.query.ticket))
-        .then((resp) => {
-            if (resp.data === undefined) return;
+router.get('/auth/redirect', async (req, res) => {
+    console.log(1);
+    await axios.get(get_ticket_validation_link(req.query.ticket))
+    .then((resp) => {
+        if (resp.data === undefined) {
+            return res.status(400).send('Invalid response from CAS server');
+        }
+    
+        const parser = new XMLParser();
 
-            const parser = new XMLParser();
+        const results = parser.parse(resp.data);
+        const userId = results['cas:serviceResponse']['cas:authenticationSuccess']['cas:user'];
 
-            let results = parser.parse(resp.data)
-            let userId = results['cas:serviceResponse']['cas:authenticationSuccess']['cas:user']
-            console.log(userId)
-    })
+        req.session.user = userId;
+        console.log(userId);
+
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 var express = require("express");
 var path = require("path");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
-var index = require("./routes/index");
-var data = require("./routes/data");
-var auth = require("./routes/auth");
 const cors = require('cors');
 const mongoose = require('mongoose');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const MongoStore = require('connect-mongo');
+
+// Routes
+const authMiddleware = require('./middleware/authMiddleware');
+var index = require("./routes/index");
+var data = require("./routes/data");
+var comment = require("./routes/comment");
+var comments = require("./routes/comments");
+var auth = require("./routes/auth");
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI);
@@ -23,40 +32,72 @@ mongoose.connection.on('disconnected', () => {
 	console.log('MongoDB disconnected');
 });
 
-var index = require("./routes/index");
-var data = require("./routes/data");
-var comment = require("./routes/comment");
-var comments = require("./routes/comments");
 
 var app = express();
-
 var port = 8081;
 
 var socket_io = require("socket.io");
-
 var io = socket_io();
 app.use(cors());
 
-//views
 
+// Session
+app.use(session({
+	secret: "yaleclubs",
+	// store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI}),
+	saveUninitialized: false,
+	resave: false
+}));
+
+
+app.use((req, res, next) => {
+	console.log(req.url);
+	if (req.url.startsWith('/login')) {
+		next();
+	}
+
+	if (req.url.startsWith('/api/auth/redirect')) {
+		next();
+	}
+  
+	authMiddleware(req, res, next);
+});
+
+// Proxies
+app.use((req, res, next) => {
+	if (req.url.startsWith('/api')) {
+		next();
+	}
+	
+	console.log(req.url);
+	createProxyMiddleware({
+		target: 'http://localhost:8082',
+		changeOrigin: true,
+	})(req, res, next);
+});
+
+
+// Use on the deployment
+// app.use(express.static(path.join(__dirname, "dist")));
+
+
+// Views
 app.set("views",  path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-//Body parser MW
-
+// Body parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-//Routes
-
+// Routes
 app.use("/", index);
 app.use("/api", data);
 app.use("/api", comment);
 app.use("/api", comments);
 app.use("/api", auth);
 
-
+// Server Listeners
 io.listen(app.listen(port, function(){
 	console.log("Server running on port", port);
 }));
