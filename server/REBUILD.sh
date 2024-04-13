12f7e978679aee6c9a67d311a72d1db3711ff91d
Ean244 <ianlim224@gmail.com>
2024-04-13 19:01:28 -0400

    improved script + bug fix

--- server/REBUILD.sh
+++ server/REBUILD.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+if [ -f "dist" ]; then
+  rm -rf dist
+fi
+
 cd ../client || exit
 
 npm install
 
-if not [ -f ".env" ]; then
+if [ -f ".env" ]; then
   echo ".env file not found!"
   exit
 fi
