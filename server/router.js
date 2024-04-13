5dfc4ae52012f0ccf94c33a017432ca6e87a5080
Ean244 <ianlim224@gmail.com>
2024-04-13 19:01:28 -0400

    improved script + bug fix

--- server/router.js
+++ server/router.js
@@ -12,7 +12,6 @@ const delete_club = require("./routes/delete");
 const getRoutes = (router) => {
     // do a blanket barrier on all routes except login
 
-
     router.get('/', async (req, res, next) => {
         if (req.session && req.session.user) {
             next();
@@ -21,18 +20,18 @@ const getRoutes = (router) => {
         }
     });
 
-    router.get("/api", data);
-    // router.get("/api", events);
+    router.use("/api", data);
+    // router.use("/api", events);
     // router.get("/api", comment);
     // router.get("/api", comments);
     // router.get("/api", subscribe);
 
-    router.get("/api", auth);
+    router.use("/api", auth);
     // router.get("/api", logout);
 
-    router.get("/api", crm);
-    router.get("/api", save_club);
-    router.get("/api", delete_club);
+    router.use("/api", crm);
+    router.use("/api", save_club);
+    router.use("/api", delete_club);
 
     return router;
 }
