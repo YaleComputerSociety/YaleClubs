61a55f592ddfae44211315c870796bad6660d96d
Ean244 <ianlim224@gmail.com>
2024-04-13 17:58:28 -0400

    added npm install to script

--- server/REBUILD.sh
+++ server/REBUILD.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 cd ../client || exit
+
+npm install
+
 if not [ -f ".env" ]; then
   echo ".env file not found!"
   exit
 fi
+
 npx expo export --platform web
 cp -r dist ../server/dist
