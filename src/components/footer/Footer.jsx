628b5a5a296cca8dd7c6808c47050970414c6e00
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-01 14:27:23 -0400

    Begin next migration and migrate main page header

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+
+const Footer = () => {
+
+    return (
+        <div className="
+            w-full 
+            absolute 
+            bottom-0 
+            bg-white 
+            p-5 flex
+            items-center
+            justify-center
+        ">
+            <div className='text-gray-300'>Copyright Reserved © {new Date().getFullYear()} Yale Clubs</div>
+        </div>
+    );
+
+}
+
+export default Footer;
