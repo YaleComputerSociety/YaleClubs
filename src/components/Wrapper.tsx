60a594540827878cba49d27ce1aceea3bdc4d919
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-07 17:39:30 -0500

    Add prettier

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,18 +1,15 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 type WrapperProps = {
   children: ReactNode;
 };
 
-
-const Wrapper = ({ children }: WrapperProps) => {    
-    return (
-        <div className="py-10 ph:mt-14 md:mt-20 w-full flex items-center">
-            <div className="w-full lg:w-[920px]">
-                {children}
-            </div>
-        </div>
-    );
+const Wrapper = ({ children }: WrapperProps) => {
+  return (
+    <div className="py-10 ph:mt-14 md:mt-20 w-full flex items-center">
+      <div className="w-full lg:w-[920px]">{children}</div>
+    </div>
+  );
 };
 
 export default Wrapper;
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,14 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable');
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 /* eslint-disable no-var */
 declare global {
-    var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null };
+  var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null };
 }
 /* eslint-enable no-var */
 
@@ -17,18 +17,18 @@ global.mongoose = global.mongoose || { conn: null, promise: null };
 const cached = global.mongoose;
 
 async function connectToDatabase(): Promise<Mongoose> {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-            return mongoose;
-        });
-    }
-
-    cached.conn = await cached.promise;
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectToDatabase;
