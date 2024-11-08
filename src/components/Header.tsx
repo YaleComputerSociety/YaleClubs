f9198a9a858599fbfb7c773564e258e3040f6004
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-08 17:21:22 -0500

    Update header

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+import LogoSVG from "../../public/assets/logo";
+
+const Header = () => {
+  return (
+    <div className="w-full flex flex-row fixed z-10 justify-between py-5 ph:px-5 px-10 md:px-[110px]">
+      <Link href="/" className="flex flex-row items-center">
+        <LogoSVG />
+        <div className="ml-5 font-semibold text-[16px]">YaleClubs</div>
+      </Link>
+
+      <div className="flex flex-row items-center justify-center">
+        <div className="ph:hidden sm:flex flex-row items-center gap-x-11 ">
+          <Link href="/About">
+            <div className="text-md">About</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
