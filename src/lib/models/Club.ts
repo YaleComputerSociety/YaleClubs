0bb20d78d81f36edd1fb5a51addd2de4459939fb
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-08 16:45:36 -0500

    Update schema

--- src/lib/models/Club.ts
+++ src/lib/models/Club.ts
@@ -1,37 +1,92 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { ILogo } from "./Logo";
+// these are just examples, we will change these later
+export enum ClubCategory {
+  SPORTS = "Sports",
+  ARTS = "Arts",
+  TECHNOLOGY = "Technology",
+  MUSIC = "Music",
+}
 
-// Interface for Club Schema
+export interface ClubLeader {
+  email: string;
+  name?: string;
+  year?: number;
+  netId?: string;
+  profilePicture?: string;
+}
+
+export interface Meeting {
+  day: string;
+  time?: string;
+  location?: string;
+}
+
+// Use this when creating/updating a club
+export interface IClubInput {
+  name: string;
+  description: string;
+  categories: ClubCategory[];
+  clubLeaders: ClubLeader[];
+  logo?: string;
+  backgroundImage?: string;
+  numMembers?: number;
+  website?: string;
+  email?: string;
+  instagram?: string;
+  phone?: string;
+  applyForm?: string;
+  mailingForm?: string;
+  meeting?: Meeting;
+}
+
+// Use this when fetching a club
 export interface IClub extends Document {
   _id: string;
-  clubName: string;
+  name: string;
   description: string;
-  instagram: string;
-  email: string;
-  phone: string;
-  website: string;
-  applyForm: string;
-  yaleConnect: string;
-  clubMembers: string[];
-  clubLeaders: string[];
-  logo: ILogo["_id"];
-  categories: string[];
+  categories: ClubCategory[];
+  clubLeaders: ClubLeader[];
+  logo?: string;
+  backgroundImage?: string;
+  numMembers?: number;
+  website?: string;
+  email?: string;
+  instagram?: string;
+  phone?: string;
+  applyForm?: string;
+  mailingForm?: string;
+  meeting?: Meeting;
 }
 
 // Club Schema
 const clubSchema = new Schema<IClub>({
-  clubName: { type: String, required: true },
+  name: { type: String, required: true },
   description: { type: String, required: true },
-  instagram: { type: String },
-  email: { type: String },
-  phone: { type: String },
+  categories: { type: [String], enum: Object.values(ClubCategory), required: true },
+  clubLeaders: [
+    {
+      type: {
+        name: { type: String, required: true },
+        role: { type: String },
+      },
+      required: true,
+    },
+  ],
+  logo: { type: String },
+  backgroundImage: { type: String },
+  numMembers: { type: Number },
   website: { type: String },
+  email: { type: String },
+  instagram: { type: String },
+  phone: { type: String },
   applyForm: { type: String },
-  yaleConnect: { type: String },
-  clubMembers: { type: [String] },
-  clubLeaders: { type: [String] },
-  logo: { type: Schema.Types.ObjectId, ref: "Logo" },
-  categories: { type: [String] },
+  mailingForm: { type: String },
+  meeting: {
+    type: {
+      location: { type: String },
+      time: { type: String },
+    },
+  },
 });
 
 const Club = mongoose.models.Club || mongoose.model<IClub>("Club", clubSchema);
