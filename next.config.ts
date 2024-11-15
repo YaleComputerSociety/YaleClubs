b42c2f61249b96925d09904054f5dbd2a2e79b93
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-15 11:09:33 -0500

    Create v2 Card header including logo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["yaleconnect.yale.edu"],
+  },
 };
 
 export default nextConfig;
