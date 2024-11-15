e938026ddf18e08ce7fdad5b7d03be40cf4574d6
Lauren Lee <laurenlee@mickey-10.local>
2024-11-15 18:04:47 -0500

    fixed search, added filter button, fix about page

---
+++ src/components/catalog/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface SearchBarProps {
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchBar = ({ searchQuery, setSearchQuery }: SearchBarProps) => {
+  return (
+    <input
+      type="text"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search..."
+      className="w-full px-4 py-2 border rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+};
+
+export default SearchBar;
---
+++ src/components/catalog/SearchControl.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import FilterButton from "./FilterButton";
+import { IClub } from "@/lib/models/Club";
+
+interface SearchControlProps {
+  allGroups: IClub[];
+  setFilteredGroups: React.Dispatch<React.SetStateAction<IClub[]>>;
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  selectedCategories: string[];
+  setSelectedCategories: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const SearchControl = ({
+  allGroups,
+  setFilteredGroups,
+  searchQuery,
+  setSearchQuery,
+  selectedCategories,
+  setSelectedCategories,
+}: SearchControlProps) => {
+  return (
+    <div className="flex flex-col md:flex-row items-start md:items-center gap-4 mb-8">
+
+      <div className="w-full md:w-[600px]">
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      </div>
+
+      <div className="md:w-auto w-full">
+        <FilterButton
+          allGroups={allGroups}
+          setFilteredGroups={setFilteredGroups}
+          selectedCategories={selectedCategories}
+          setSelectedCategories={setSelectedCategories}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchControl;
