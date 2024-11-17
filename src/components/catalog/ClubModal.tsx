ce5fda8a142a11aa581a43f80fcefd8d2be0eb8a
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-17 14:26:53 -0500

    Implement title, categories, affiliation, and description for modal

--- src/components/catalog/ClubModal.tsx
+++ src/components/catalog/ClubModal.tsx
@@ -7,10 +7,10 @@ type ClubModalProps = {
 };
 
 const ClubModal = ({ club, onClose }: ClubModalProps) => {
+  console.log(club);
   const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Close modal on pressing Escape key
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") onClose();
     };
@@ -28,22 +28,56 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, [onClose]);
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div ref={modalRef} className="relative bg-white rounded-lg max-w-3xl w-full mx-5 md:mx-auto h-5/6">
+      <div
+        ref={modalRef}
+        className="relative bg-white rounded-lg max-w-3xl w-full mx-5 md:mx-auto h-5/6 overflow-hidden"
+      >
         <img
           src={club.backgroundImage || "/assets/default-background.png"}
           alt="Club Background"
           className="w-full h-48 object-cover rounded-t-lg mb-4"
         />
-        {/* <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-2xl text-gray-600 hover:text-gray-800 focus:outline-none"
-        >
-          &times;
-        </button> */}
-        <h2 className="text-2xl font-bold mb-4">{club.name}</h2>
-        <p className="text-gray-700 mb-4">{club.description}</p>
+        <div className="flex flex-col md:flex-row m-4 gap-8 h-[calc(100%-14rem)] overflow-y-auto">
+          <div className="flex flex-col md:w-2/3">
+            <div className={`${club.name.length > 100 ? "text-2xl" : "text-3xl"} font-bold mb-4`}>{club.name}</div>
+            <div className="flex gap-2 whitespace-nowrap w-full flex-wrap">
+              {club.categories.length > 0 &&
+                club.categories.map((tag, index) => (
+                  <span key={index} className="bg-[#eee] rounded px-2 py-1 text-sm">
+                    {tag}
+                  </span>
+                ))}
+            </div>
+            {club.affiliation && (
+              <div className="w-fit bg-[#fdf] rounded px-2 py-1 text-sm mt-4">{club.affiliation}</div>
+            )}
+            <div className="text-gray-700 mt-4">{club.description}</div>
+            {club.leaders.length > 0 && (
+              <div className="mt-4">
+                <div className="text-lg font-bold">Board</div>
+                {club.leaders.map((leader, index) => (
+                  <div key={index} className="flex gap-4 items-center mt-2">
+                    <div>
+                      <div className="text-lg font-semibold">{leader.name}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <div className="text-gray-700 mb-4">something</div>
+          </div>
+        </div>
       </div>
     </div>
   );
