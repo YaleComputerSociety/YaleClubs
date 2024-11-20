4941fde43fbe8cd8836c1c0aa748e9f3b95d46f4
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-20 15:50:12 -0500

    Modify mobile modal view to resize elements and move profile picture

--- src/components/catalog/ClubModal.tsx
+++ src/components/catalog/ClubModal.tsx
@@ -3,6 +3,7 @@ import { IClub } from "@/lib/models/Club";
 import Board from "./Board";
 import Image from "next/image";
 import { getAdjustedNumMembers } from "@/lib/utils";
+import { useMediaQuery } from "react-responsive";
 
 type ClubModalProps = {
   club: IClub;
@@ -11,6 +12,7 @@ type ClubModalProps = {
 
 const ClubModal = ({ club, onClose }: ClubModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -41,19 +43,32 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div
         ref={modalRef}
-        className="relative bg-white rounded-lg max-w-3xl w-full mx-5 md:mx-auto h-5/6 overflow-hidden"
+        className="relative bg-white rounded-lg max-w-3xl w-full mx-4 md:mx-auto h-5/6 overflow-hidden flex flex-col"
       >
         <Image
           src={club.backgroundImage || "/assets/default-background.png"}
           alt="Club Background"
-          className="w-full h-1/4 md:h-48 object-cover rounded-t-lg mb-4"
+          className="w-full h-1/4 md:h-48 object-cover rounded-t-lg"
           width={768}
           height={192}
         />
-        <div className="flex flex-col h-[calc(100%-14rem)] overflow-y-auto overflow-x-hidden m-4">
+        {isMobile && (
+          <Image
+            src={club.logo ?? "/assets/default-logo.png"}
+            alt="Club Logo"
+            width={100}
+            height={100}
+            className={`${club.logo ? "rounded-full" : ""} flex-shrink-0 border-2 border-white relative -top-[50px] mx-auto`}
+          />
+        )}
+        <div className="flex flex-col overflow-y-auto overflow-x-hidden m-4 -mt-[50px] md:mt-4">
           <div className="flex flex-col md:flex-row gap-4">
             <div className="flex flex-col md:w-3/5">
-              <div className={`${club.name.length > 100 ? "text-2xl" : "text-3xl"} font-bold`}>{club.name}</div>
+              <div
+                className={`text-center md:text-left ${club.name.length > 100 ? "text-xl md:text-2xl" : "text-2xl md:text-3xl"} font-bold`}
+              >
+                {club.name}
+              </div>
               {club.categories && club.categories.length > 0 && (
                 <div className="flex gap-2 whitespace-nowrap w-full flex-wrap mt-4">
                   {club.categories.map((tag, index) => (
@@ -68,18 +83,20 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
               ) : (
                 <div className="text-gray-700 text-md mt-4">Unknown affiliation</div>
               )}
-              <div className="text-gray-700 mt-4">{club.description || "No description"}</div>
+              <div className="text-gray-700 mt-4 text-sm sm:text-base">{club.description || "No description"}</div>
             </div>
             <div className="flex flex-col md:w-2/5 items-center">
-              <div className="flex flex-col items-center gap-4">
-                <Image
-                  src={club.logo ?? "/assets/default-logo.png"}
-                  alt="Club Logo"
-                  width={100}
-                  height={100}
-                  className={`${club.logo ? "rounded-full" : ""} flex-shrink-0`}
-                />
-              </div>
+              {!isMobile && (
+                <div className="flex flex-col items-center gap-4">
+                  <Image
+                    src={club.logo ?? "/assets/default-logo.png"}
+                    alt="Club Logo"
+                    width={100}
+                    height={100}
+                    className={`${club.logo ? "rounded-full" : ""} flex-shrink-0`}
+                  />
+                </div>
+              )}
               <div className="flex flex-col w-full sm:w-3/4 md:w-full">
                 {club.website && (
                   <div className="flex flex-row justify-between gap-2 text-sm mt-4 font-semibold">
