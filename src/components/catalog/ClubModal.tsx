68ad7e0b424d37a8c97911262a1f7f7cf0dc8724
Goolsbee Addison <awg32@zoo.cs.yale.edu>
2024-12-04 00:49:52 -0500

    Fix logo borders, add school

--- src/components/catalog/ClubModal.tsx
+++ src/components/catalog/ClubModal.tsx
@@ -44,7 +44,7 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div
         ref={modalRef}
-        className="relative bg-white rounded-lg max-w-3xl w-full mx-4 md:mx-auto h-5/6 overflow-hidden flex flex-col"
+        className="relative bg-white rounded-lg max-w-3xl w-full mx-4 md:mx-auto h-5/6 max-h-[1000px] overflow-hidden flex flex-col"
       >
         <Image
           src={club.backgroundImage || "/assets/default-background.png"}
@@ -59,7 +59,7 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
             alt="Club Logo"
             width={100}
             height={100}
-            className={`${club.logo ? "rounded-full" : ""} flex-shrink-0 border-2 border-white relative -top-[50px] mx-auto`}
+            className={`${club.logo ? "rounded-2xl" : ""} flex-shrink-0 border-2 border-white relative -top-[50px] mx-auto`}
           />
         )}
         <div className="flex flex-col overflow-y-auto overflow-x-hidden m-4 -mt-[50px] md:mt-4">
@@ -70,6 +70,20 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
               >
                 {club.name}
               </div>
+              {club.school && (
+                <div className="flex gap-2 whitespace-nowrap w-full flex-wrap mt-4">
+                  <span className="bg-[#ccf] rounded px-2 py-1 text-sm">{club.school}</span>
+                </div>
+              )}
+              {club.affiliations && club.affiliations.length > 0 && (
+                <div className="flex gap-2 whitespace-nowrap w-full flex-wrap mt-4">
+                  {club.affiliations.map((tag, index) => (
+                    <span key={index} className="bg-[#fdf] rounded px-2 py-1 text-sm">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              )}
               {club.categories && club.categories.length > 0 && (
                 <div className="flex gap-2 whitespace-nowrap w-full flex-wrap mt-4">
                   {club.categories.map((tag, index) => (
@@ -78,11 +92,6 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
                     </span>
                   ))}
                 </div>
-              )}
-              {club.affiliations && club.affiliations.length > 0 ? (
-                <div className="w-fit bg-[#fdf] rounded px-2 py-1 text-sm mt-4">{club.affiliations[0]}</div>
-              ) : (
-                <div className="text-gray-700 text-md mt-4">Unknown affiliation</div>
               )}
               <div className="text-gray-700 mt-4 text-sm sm:text-base">{club.description || "No description"}</div>
             </div>
@@ -94,7 +103,7 @@ const ClubModal = ({ club, onClose }: ClubModalProps) => {
                     alt="Club Logo"
                     width={100}
                     height={100}
-                    className={`${club.logo ? "rounded-full" : ""} flex-shrink-0`}
+                    className={`${club.logo ? "rounded-2xl" : ""} flex-shrink-0`}
                   />
                 </div>
               )}
