d97d8de9a295b8af4be9452882a0267f66aee119
Lauren Lee <laurenwyl@gmail.com>
2024-12-04 12:38:19 -0500

    Test

--- src/components/catalog/AffiliationFilter.tsx
+++ src/components/catalog/AffiliationFilter.tsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import { IClub } from "@/lib/models/Club";
 import { Affiliation } from "@/lib/models/Club";
 
-
 interface AffiliationFilterButtonProps {
   setFilteredGroups: React.Dispatch<React.SetStateAction<IClub[]>>;
   selectedAffiliations: string[];
