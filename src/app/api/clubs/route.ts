763c1cc074766e5832f1b22ad28ee0ea99dc3773
Quintec Music <quintek55555@gmail.com>
2024-12-06 00:29:00 -0500

    feat: authz middleware, guard delete

--- src/app/api/clubs/route.ts
+++ src/app/api/clubs/route.ts
@@ -20,8 +20,13 @@ export async function POST(req: Request): Promise<NextResponse> {
     // Connect to the database
     await connectToDatabase();
 
-    // Parse the incoming JSON body
-    const body: IClubInput = await req.json();
+    let body: IClubInput;
+    try {
+      body = await req.json();
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+    }
 
     // Validate required fields
     if (!body.name || !body.leaders || !Array.isArray(body.leaders)) {
@@ -69,6 +74,11 @@ export async function DELETE(req: Request): Promise<NextResponse> {
   try {
     // Connect to the database
     await connectToDatabase();
+
+    let netid = req.headers.get("X-NetID");
+    if (netid !== "admin_a1b2c3e") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
     // Get the club ID from the query parameters
     const url = new URL(req.url);
---
+++ src/middleware.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
+
+async function decodeJWTToken(token: string): Promise<any> {
+  const JWT_SECRET = process.env.JWT_SECRET as string;
+  if (!JWT_SECRET) {
+    throw new Error("Please define the JWT_SECRET environment variable");
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
+    return payload;
+  } catch (e) {
+    console.error("Invalid JWT token: " + e);
+    return null;
+  }
+}
+
+export async function middleware(request: NextRequest) {
+  console.log("middleware");
+  if (request.method !== "POST") {
+    return NextResponse.next();
+  }
+
+  const authHeader = request.headers.get("Authorization");
+  if (!authHeader) {
+    return NextResponse.json({ error: "Authorization required" }, { status: 401 });
+  }
+  if (!authHeader.startsWith("Bearer ")) {
+    return NextResponse.json({ error: "Invalid Authorization format" }, { status: 401 });
+  }
+
+  const token = authHeader.split(" ")[1];
+  const decoded = await decodeJWTToken(token);
+  if (!decoded) {
+    return NextResponse.json({ error: "Invalid token" }, { status: 401 });
+  }
+
+  const response = NextResponse.next();
+  response.headers.set("X-NetID", JSON.stringify(decoded.netid));
+  response.headers.set("X-Email", JSON.stringify(decoded.email));
+  return response;
+}
+
+export const config = {
+  matcher: ["/api/clubs/:path*"],
+};
