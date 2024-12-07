6b414047062610116f05956dffc0eb8da9e8d52d
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-12-06 19:44:06 -0500

    Refactor filter into single component

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -12,7 +12,7 @@ const SearchBar = ({ searchQuery, setSearchQuery }: SearchBarProps) => {
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
       placeholder="Search..."
-      className="px-4 py-2 w-full md:w-auto border rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer h-9 md:h-11"
+      className="px-4 py-2 w-full sm:w-80 border rounded-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer h-9 md:h-11"
     />
   );
 };
