ead5e2745f5d1612f6156eaf03d46fb0570b15b0
Ethan M <ethan.mathieu@yale.edu>
2024-12-21 00:06:12 -0500

    schema draft

---
+++ src/lib/models/Events.ts
@@ -0,0 +1,66 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IClub } from "./Club";
+
+export enum Tag {
+  FreeFood = "Free Food",
+  LimitedCapacity = "Limited Capacity",
+  StartsPromptly = "Starts Promptly",
+  RegistrationRequired = "Registration Required",
+  Showcase = "Showcase",
+  Performance = "Performance",
+  Athletics = "Athletics",
+  CommunityService = "Community Service",
+  CommunityBuilding = "Community Building",
+  Speaker = "Speaker",
+  Social = "Social",
+  Party = "Party",
+  GoodForBeginners = "Good for Beginners",
+  GoodForNewMembers = "Good for New Members",
+  ProfessionalDevelopment = "Professional Development",
+}
+
+export interface IEventInput {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string;
+
+  name: string;
+  description?: string;
+  club: IClub;
+  start: Date;
+  location: string;
+  registrationLink?: string;
+  flyer?: string;
+  tags?: Tag[];
+}
+
+export interface IEvent extends Document {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string;
+
+  name: string;
+  description: string;
+  club: string;
+  start: Date;
+  location: string;
+  registrationLink?: string;
+  flyer?: string;
+  tags?: Tag[];
+}
+
+const eventSchema = new Schema<IEvent>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  club: { type: String, required: true },
+  start: { type: Date, required: true },
+  location: { type: String, required: true },
+  registrationLink: { type: String, required: false },
+  flyer: { type: String, required: false },
+  tags: { type: [String], enum: Object.values(Tag), default: [] },
+});
+
+const Event = mongoose.models?.Event || mongoose.model<IEvent>("Event", eventSchema);
+export default Event;
