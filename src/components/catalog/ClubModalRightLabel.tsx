07aa90536ae7bb7a9565cbc2404d22acf226656d
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-01-08 21:15:24 -0600

    Fix modal right side bugs and typos

--- src/components/catalog/ClubModalRightLabel.tsx
+++ src/components/catalog/ClubModalRightLabel.tsx
@@ -13,7 +13,7 @@ const ClubModalRightLabel = ({ header, content, link }: Props) => {
     <div className="flex flex-row justify-between gap-6 text-sm mt-4 font-semibold items-start">
       <div className="text-gray-500">{header}</div>
       {link ? (
-        <a className="text-blue-500 text-right break-all min-w-0" href={link}>
+        <a className="text-blue-500 text-right break-all min-w-0" href={link} target="_blank" rel="noopener noreferrer">
           {content}
         </a>
       ) : (
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,20 @@
 export const getAdjustedNumMembers = (numMembers: number): string => {
-  if (numMembers <= 10) {
-    return "1 - 10";
-  } else if (numMembers <= 25) {
-    return "10 - 25";
+  if (numMembers <= 15) {
+    return "1 - 15";
+  } else if (numMembers <= 30) {
+    return "15 - 30";
   } else if (numMembers <= 50) {
-    return "25-50";
+    return "30 - 50";
   } else if (numMembers <= 100) {
-    return "50-100";
+    return "50 - 100";
   } else {
     return "100+";
   }
+};
+
+export const getAdjustedWebsite = (website: string | undefined): string => {
+  if (!website) {
+    return "No website";
+  }
+  return website.replace(/(^\w+:|^)\/\//, "");
 };
