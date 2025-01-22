aa24a4d8c8c3f79b8307cadd225a49d00e4a1a56
Ethan M <ethan.mathieu@yale.edu>
2025-01-22 14:11:58 -0500

    edited about page

--- src/components/events/constants.ts
+++ src/components/events/constants.ts
@@ -1,7 +1,7 @@
 export const NAME_MAX_LENGTH = 20;
 export const NAME_MIN_LENGTH = 7;
 export const DESCRIPTION_MAX_LENGTH = 500;
-export const DESCRIPTION_MIN_LENGTH = 50;
+export const DESCRIPTION_MIN_LENGTH = 25;
 export const LOCATION_MAX_LENGTH = 100;
 export const LOCATION_MIN_LENGTH = 2;
 export const MAX_NUMBER_OF_EVENTS_PER_MONTH = 3;
