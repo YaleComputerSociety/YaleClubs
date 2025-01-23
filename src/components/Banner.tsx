211db4795b4c8a54ec3b9fd18b16af320aaaad68
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-01-22 20:39:39 -0500

    Adjust banner sizing

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -22,7 +22,7 @@ const Banner = ({ onHeightChange }: { onHeightChange: (height: number) => void }
   }, []);
 
   useEffect(() => {
-    // localStorage.clear(); // MAKE SURE TO COMMENT THIS OUT WHEN COMMITTING
+    localStorage.clear(); // MAKE SURE TO COMMENT THIS OUT WHEN COMMITTING
     const isClosed = localStorage.getItem("bannerClosed");
     if (!isClosed) {
       const timer = setTimeout(() => {
@@ -46,50 +46,27 @@ const Banner = ({ onHeightChange }: { onHeightChange: (height: number) => void }
   return (
     <div
       ref={bannerRef}
-      className={`fixed ${isMobile ? "py-4" : "py-5"} top-0 left-0 right-0 z-50 bg-clubPurple text-white transition-transform duration-1000 ${
+      className={`fixed p-2 sm:p-0 top-0 left-0 right-0 z-50 bg-clubPurple text-white transition-transform duration-1000 ${
         isVisible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
-      {isMobile ? (
-        <div className="flex justify-center gap-5 px-5 py-0 text-l">
-          <p>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSdBM9ccbynx2eQKVdCkpPDW-sIJArTWqUlMGGKuXz175iq0Og/viewform"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline"
-            >
-              We want your feedback! Take a quick survey to help us improve.
-            </a>
-          </p>
-          <button onClick={handleClose} className="text-white hover:text-gray-300 focus:outline-none px-2 text-4xl">
-            &times;
-          </button>
-        </div>
-      ) : (
-        <div className="flex items-center justify-between px-0 py-2">
-          {/* Centered message */}
-          <div className="text-center flex-grow text-xl">
-            <p>
-              <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLSdBM9ccbynx2eQKVdCkpPDW-sIJArTWqUlMGGKuXz175iq0Og/viewform"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                We want your feedback! Take a quick <span className="underline">survey</span> to help us improve.
-              </a>
-            </p>
-          </div>
-
-          {/* Close button */}
-          <button
-            onClick={handleClose}
-            className="text-white hover:text-gray-300 focus:outline-none px-2 text-4xl mr-4"
+      <div className={`flex ${isMobile ? "justify-center gap-3 px-5" : "items-center justify-between px-0"} py-2`}>
+        <p className={`flex-grow ${isMobile ? "" : "text-center text-xl"}`}>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdBM9ccbynx2eQKVdCkpPDW-sIJArTWqUlMGGKuXz175iq0Og/viewform"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            &times;
-          </button>
-        </div>
-      )}
+            We want your feedback! Take a quick <span className="underline">survey</span> to help us improve.
+          </a>
+        </p>
+        <button
+          onClick={handleClose}
+          className="text-white hover:text-gray-300 focus:outline-none text-4xl px-2 sm:mr-4"
+        >
+          &times;
+        </button>
+      </div>
     </div>
   );
 };
