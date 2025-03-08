43c855461b3eca4ab6550df4e8285d8785440a99
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-03-08 14:09:44 -0500

    Fix search bar text

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearchBar = ({ searchQuery, setSearchQuery }: SearchBarProps) => {
       type="text"
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
-      placeholder="Search by club name, acronym, etc."
+      placeholder="Search..."
       className="px-4 py-2 w-full sm:w-80 lg:w-96 xl:w-[32rem] border rounded text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text h-11"
     />
   );
