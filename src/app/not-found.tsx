a86022493e8c8dcc356261bed481cf95840e3e46
Francis Fan <yfan@Franciss-MacBook-Pro-3.local>
2025-04-13 14:03:15 -0400

    made color coherent

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,8 +9,8 @@ export default function NotFound() {
     <main className=" flex flex-col min-h-screen">
       <div className="flex-grow">
         <Header />
-        <div className="flex flex-col items-center justify-center min-h-screen text-center p-6 bg-gray-50">
-          <Image src="/assets/icon.png" alt="YaleClubs Logo" width={160} height={160} className="mb-6 animate-bounce" />
+        <div className="flex flex-col items-center justify-center min-h-screen text-center p-6 bg-background">
+          <Image src="/assets/logo.svg" alt="YaleClubs Logo" width={160} height={160} className="mb-6 animate-bounce" />
           <h1 className="text-5xl font-extrabold text-gray-800 mb-4">404 - Page Not Found</h1>
           <p className="mb-6 text-xl text-gray-600">Oops! The page you are looking for does not exist.</p>
           <Link href="/" className="text-blue-500 hover:underline">
