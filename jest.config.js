884ee09362ffd366817ce2fe418c23cb8514bdcc
emmett <exseto22@gmail.com>
2026-02-02 19:15:05 -0500

    Add Jest testing framework and utility function tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
